adcb2115cca7fb0685528034679c4f5aa7d07f4e
Arthur Figueiredo <arthurfn.webdev@gmail.com>
2022-08-20 14:53:17 -0300

    Add ConsoleChannel and ApiBot

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import { VenomBotChannel } from "./channels/venom-bot/VenomBotChannel";
-import { FirstBot } from "./bot/samples/FirstBot";
+import { JsonBot } from "./bots/JsonBot";
 import { ChannelConfig } from "./core/config/ChannelConfig";
 
 const venomBot: VenomBotChannel = new VenomBotChannel();
 
 const config = new ChannelConfig();
-config.bot = new FirstBot();
+config.bot = new JsonBot();
 venomBot.create(config);
